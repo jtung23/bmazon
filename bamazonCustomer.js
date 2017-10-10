fbb78d36e41368f316e67f33dc547a007faa2be0
Jonathan Tung <jtung23@gmail.com>
2017-10-09 18:49:31 -0700

    first part done

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,7 +4,8 @@
 
 var sql = require('mysql');
 var inquirer = require('inquirer');
-var quantArray = ['skip']
+var quantArray = ['skip'];
+var priceArray = ['skip'];
 var connection = sql.createConnection({
 host: 'localhost',
 user: 'root',
@@ -28,14 +29,17 @@ connection.query('SELECT * FROM `products`',
 				console.log("ID: " + results[i].item_id, "Product: " + results[i].product_name,
 					"Price: $" + results[i].price);
 				quantArray.push(results[i].stock_quantity)
+				priceArray.push(results[i].price)
 			};
 			inquireIdQuantity();
 	}
 );
-
-
-
-var inquireIdQuantity = function() {
+// to use callback and use values from inquireidquantity.
+// 			inquireIdQuantity(welrkja)
+// inquireIdQuantity(function (price) {
+/// do sojething with quanitity
+// }); 
+var inquireIdQuantity = function(callback) {
 	inquirer
 		.prompt([{
 			type: 'input',
@@ -55,27 +59,41 @@ var inquireIdQuantity = function() {
 		.then(function(results, err) {
 			var id = results.whatId;
 			var promptQuantity = results.whatQuantity;
+
 			if (results.buy) {
-				quantityCompare(id, promptQuantity);
+				quantityCompare(id, promptQuantity, removeQuantitySql);
 			} else {
 				inquireIdQuantity();
+				// callback(results.price)
 			}
 			
 		})
 }
+// updates quantity in database
+var removeQuantitySql = function(newQuantity, id, promptQuantity, unitPrice) {
+	var totalCost = promptQuantity * unitPrice;
+
+	connection.query("UPDATE products SET stock_quantity='" + newQuantity + "' WHERE item_id='" + id + "'",
+		function(error, results) {
+			if (error) throw error;
+			console.log('Database quantity updated');
+			console.log('Your total cost is: $' + totalCost);
+		}
+	)
+}
 
 // FUNCTION
 // compares sql quantity to prompt quantity
-var quantityCompare = function(id, promptQuantity) {
-	console.log(id);
-	console.log(promptQuantity);
-	console.log(quantArray);
-
-	console.log(quantArray[id]);
+var quantityCompare = function(id, promptQuantity, callback) {
 
 	if (quantArray[id] < promptQuantity) {
-		console.log('less')
+		// quantity less then wanted quantity
+		console.log('Insufficient quantity!')
+		inquireIdQuantity();
 	} else {
-		console.log('more')
+		console.log('Purchased!');
+		var newQuantity = quantArray[id] - promptQuantity;
+		var unitPrice = priceArray[id];
+		callback(newQuantity, id, promptQuantity, unitPrice);
 	}
 }
