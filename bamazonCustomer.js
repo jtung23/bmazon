ae0e1c8a4865cb77b6891f9fae92c0df7de45d73
Jonathan Tung <jtung23@gmail.com>
2017-10-09 17:06:03 -0700

    sql and node set up

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,3 +2,54 @@
 // available for sale. Include the ids, names, and prices of 
 // products for sale.
 
+var sql = require('mysql');
+var inquirer = require('inquirer');
+
+var connection = sql.createConnection({
+host: 'localhost',
+user: 'root',
+password: 'sqlpassword',
+database: 'bamazon'
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+ 
+  console.log('connected as id ' + connection.threadId);
+});
+
+connection.query('SELECT * FROM `products`',
+	function (error, results, fields) {
+		console.log(results);
+			for (var i = 0; i < results.length; i++) {
+				console.log("ID: " + results[i].item_id, "Product: " + results[i].product_name,
+					"Price: $" + results[i].price);
+			};
+
+		inquireIdQuantity();
+	}
+);
+
+
+
+var inquireIdQuantity = function() {
+	inquirer
+		.prompt([{
+			type: 'input',
+			name: 'whatId',
+			message: 'Which product Id would you like to buy?',
+		},
+		{
+			type: 'input',
+			name: 'whatQuantity',
+			message: 'How many units would you like to buy?'
+		}])
+		.then(function(results, err) {
+			console.log(results.whatId);
+			console.log(results.whatQuantity);
+			return results.whatId;
+		})
+}
